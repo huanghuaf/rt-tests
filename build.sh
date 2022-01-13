f92924b647158ae7551db1ecda25eab5447ca7d8
huafeng.huang <huafeng.huang@semidrive.com>
2022-01-13 16:31:50 +0800

    remove numactl-2.0.14 package
    Signed-off-by: huafeng.huang <huafeng.huang@semidrive.com>

--- build.sh
+++ build.sh
@@ -9,8 +9,19 @@ export AR=${CROSS_COMPILER}ar
 export STRIP=${CROSS_COMPILER}strip
 export RANLIB=${CROSS_COMPILER}ranlib
 
+if [ ! -f numactl-2.0.14.tar.gz ];then
+	wget https://github.com/numactl/numactl/releases/download/v2.0.14/numactl-2.0.14.tar.gz
+fi
+
+tar -zxf numactl-2.0.14.tar.gz
+
+if [ $? -ne 0 ];then
+	exit 1；
+fi
+
 (cd ./numactl-2.0.14;./configure --target=aarch64-linux-gnu --host=aarch64-linux-gnu --build=x86_64-pc-linux-gnu --prefix=$(pwd)/../usr --enable-static;make;make install)
 
+rm -rf numactl-2.0.14;
 
 make CROSS_COMPILE=aarch64-linux-gnu-
 
